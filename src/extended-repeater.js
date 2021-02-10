8c5f11e625c390c2a48da32f154101d457283f51
Kozazavr <sohee@yandex.ru>
2021-02-10 20:21:55 +0300

    all complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  //repeatTimes sets the number of repetitions of the str;
+  //separator is a string separating repetitions of the str;
+  //addition is an additional string that will be added to each repetition of the str;
+  //additionRepeatTimes sets the number of repetitions of the addition;
+  //additionSeparator is a string separating repetitions of the addition.
+  if (options.repeatTimes == undefined && options.additionRepeatTimes == undefined) {
+    return str + options.addition;
+  }
+  if (options.additionSeparator == undefined) {
+    options.additionSeparator = '|';
+  }
+  if (options.separator == undefined) {
+    options.separator = '+';
+  }
+  if (options.addition === null) {
+    options.addition = 'null';
+  }
+  let arrAdd = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+      arrAdd.push(options.addition);
+  }
+  let strAdd = arrAdd.join(options.additionSeparator);
+  let str2 = str + strAdd;
+  let arrStr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    arrStr.push(str2);
+  }
+   return arrStr.join(options.separator);
+
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolean) {
+    this.boolean = boolean;
+  }
+
+
+  encrypt(str, key) {
+
+     if(!str || !key) {
+      throw new Error();
+    }
+    let arr = [];
+    let strForEncrypt = str.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if (strForEncrypt.charCodeAt(i) >= '65' && strForEncrypt.charCodeAt(i) <= '90') {
+        arr[i] = String.fromCharCode((26 + strForEncrypt.charCodeAt(i) - 65 + (keyUpper.charCodeAt(j % keyUpper.length) - 65)) % 26 + 65);
+        j++;
+      } else {
+        arr[i] = strForEncrypt[i];
+      }
+    }
+    if (this.boolean != undefined) {
+      return arr.reverse().join('');
+    }
+      return arr.join('');
+
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(str, key) {
+ 
+    if(!str || !key) {
+      throw new Error();
+    }
+    let arr = [];
+    let strForDecrypt = str.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if (strForDecrypt.charCodeAt(i) >= '65' && strForDecrypt.charCodeAt(i) <= '90') {
+        arr[i] = String.fromCharCode((26 + strForDecrypt.charCodeAt(i) - 65 - (keyUpper.charCodeAt(j % keyUpper.length) - 65)) % 26 + 65);
+        j++;
+      } else {
+        arr[i] = strForDecrypt[i];
+      }
+    }
+    if (this.boolean != undefined) {
+      return arr.reverse().join('');
+    }
+       return arr.join('');
   }
 }
 
